f3ed9df1e6f9121f1411118f8a2624d0e71bfa5f
Antonin Deniau <antonin@playmoweb.com>
2023-10-28 22:03:34 +0200

    FIxes

--- utils/create_modelfile.py
+++ utils/create_modelfile.py
@@ -4,12 +4,12 @@ import jinja2
 import yaml
 
 environment = jinja2.Environment()
-template = environment.from_string(open("./Modelfile.jinja2").read())
+template = environment.from_string(open("./utils/Modelfile.jinja2").read())
 
 model_id = sys.argv[1]
 
 
-models_config = yaml.load(open(os.path.join(os.path.dirname(__file__), "./models.yml"), "r"), Loader=yaml.FullLoader)
+models_config = yaml.load(open(os.path.join(os.path.dirname(__file__), "../models.yml"), "r"), Loader=yaml.FullLoader)
 model_config = models_config["models"][model_id]
 
 sys.stdout.write(template.render({
